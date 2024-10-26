586639c001aa46f7187cee2e6136ae05ffb01898
Lily <bro21019@byui.edu>
2024-10-26 17:31:11 -0600

    Updated Blog!

--- blog/blog.js
+++ blog/blog.js
@@ -23,5 +23,38 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his 'Aunt Pol' and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
 	}
 ]
+
+function getArticle(item) {
+	return `<section class="review" id="${item.id}">
+		<section class="info">
+			<h4 class="date">${item.date}</h4>
+			<p class="ages">${item.ages}</p>
+			<p class="genre">${item.genre}</p>
+			<p class="rating">${item.rating}</p>
+		</section>
+		<section class="book">
+			<h2 class="title">${item.title}</h3>
+			<img class="cover" src="${item.imgSrc}" alt="${item.imgAlt}">
+			<p class="description">${item.description} <a class="readmore">Read More...</a></p>
+		</section>
+	</section>`
+}
+
+articles.forEach(element => {
+    content.insertAdjacentHTML('beforeend', getArticle(element));
+});
